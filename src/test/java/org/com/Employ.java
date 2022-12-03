770e5637adefa81556b699a86a9e9db317de39fa
Ashwin A <ashwinashwin5243@gmail.com>
2022-12-03 17:54:45 +0530

    message

---
+++ src/test/java/org/com/Employ.java
@@ -0,0 +1,18 @@
+package org.com;
+
+import java.lang.reflect.Method;
+
+public class Employ {
+private void metho() {
+	// TODO Auto-generated method stub
+System.out.println("1");
+}
+private void method() {
+	// TODO Auto-generated method stub
+System.out.println("2");
+}
+private void Method() {
+	// TODO Auto-generated method stub
+System.out.println("3");
+}
+}

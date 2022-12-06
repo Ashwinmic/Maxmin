8c798aa2b84a2aae32f877d55dc9dc84bae3933c
Ashwinmic <119737155+Ashwinmic@users.noreply.github.com>
2022-12-06 19:12:39 +0530

    Update Employ.java

--- src/test/java/org/com/Employ.java
+++ src/test/java/org/com/Employ.java
@@ -3,16 +3,16 @@ package org.com;
 import java.lang.reflect.Method;
 
 public class Employ {
-private void metho() {
-	// TODO Auto-generated method stub
+private void method1() {
+
 System.out.println("1");
 }
-private void method() {
-	// TODO Auto-generated method stub
+private void method2() {
+
 System.out.println("2");
 }
-private void Method() {
-	// TODO Auto-generated method stub
+private void method3() {
+	
 System.out.println("3");
 }
 }
